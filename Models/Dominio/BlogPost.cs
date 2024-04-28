7b720d25b53f4bc2d184611562788606e0e9e321
Kamila <juliana.sosa@udla.edu.ec>
2024-04-27 23:01:10 -0500

    Creación de la carpeta Dominio, y de las clases BlogPost y Tags uniendolas de muchos a muchos mediante un ICollection.

---
+++ Models/Dominio/BlogPost.cs
@@ -0,0 +1,23 @@
+﻿namespace Proyecto_Integrador_JS_DM_PZ.Models.Dominio
+{
+    public class BlogPost
+    {
+        public Guid ID { get; set; }
+
+        public string Encabezado { get; set; }
+
+        public string TituloPagina{ get; set; }
+
+        public string Contenido { get; set; }
+        public string Descripcion { get; set; }
+
+        public string ImagenUrl{ get; set; }
+        public string ManejoUrl { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+
+        public string Autor {  get; set; }
+        public bool Visibilidad { get; set; }
+
+        public ICollection<Tag> Tags { get; set; }
+    }   
+}
---
+++ Models/Dominio/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto_Integrador_JS_DM_PZ.Models.Dominio
+{
+    public class Tag
+    {
+        public Guid ID { get; set; }
+        public string Nombre { get; set; }
+        public string NombreDisplay { get; set; }
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
